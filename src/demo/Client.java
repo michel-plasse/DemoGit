5a8efa626d9005b7950fc9e8f1bfeb2bdd6008de
Michel Plasse <mplasse@free.fr>
2021-03-30 14:44:16 +0200

    Ajoute get et set de noClient dans Client

--- src/demo/Client.java
+++ src/demo/Client.java
@@ -2,4 +2,13 @@ package demo;
 
 public class Client {
   private int noClient;
+
+  public int getNoClient() {
+    return noClient;
+  }
+
+  public void setNoClient(int noClient) {
+    this.noClient = noClient;
+  }
+  
 }
