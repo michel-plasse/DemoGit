3b2ab69e1f125b521d37fdeafbd22a8c972ddddc
prouf <prouf@V.VILGENIS.MASSY>
2022-12-09 14:48:03 +0100

    Ajout de la classe Philippe l'ttribbut isStupid et suppresion de celui ci

---
+++ DemoGit/src/main/java/fr/demogit/Philippe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.demogit;
+
+/**
+ *
+ * @author prouf
+ */
+public class Philippe {
+    private int unEntier;
+    private int unAutreEntier;
+    private boolean Owo;
+    
+}
