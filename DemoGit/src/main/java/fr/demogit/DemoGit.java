53a9bfc46db74d17dc90202a813d6f4281f875b9
SALLmouhamadou <salllmouha10@gmail.com>
2022-12-09 14:25:18 +0100

    ajoute la classe Mouhamadou

---
+++ DemoGit/src/main/java/fr/demogit/DemoGit.java
@@ -0,0 +1,12 @@
+package fr.demogit;
+
+/**
+ *
+ * @author SALL Mouhamadou
+ */
+public class DemoGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ DemoGit/src/main/java/fr/demogit/Mouhamadou.java
@@ -0,0 +1,11 @@
+
+package fr.demogit;
+
+/**
+ *
+ * @author SALL Mouhamadou
+ */
+public class Mouhamadou {
+    private int age;
+    private String nom, prenom;
+}
