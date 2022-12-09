2960db1715b02bcb9c1f0bfd5270226df50b23b3
Damien <seal.dofus@gmail.com>
2022-12-09 14:56:54 +0100

    Indentation

--- src/demo/Damien.java
+++ src/demo/Damien.java
@@ -1,11 +1,11 @@
 package demo;
 
 public class Damien {
-private String truc = "qui me concerne";
-private final int quaranteDeux = 42;
-private int po = 40;
+	private String truc = "qui me concerne";
+	private final int quaranteDeux = 42;
+	private int po = 40;
 
-public int getQuaranteDeux() {
-	return quaranteDeux;
-}
+	public int getQuaranteDeux() {
+		return quaranteDeux;
+	}
 }
